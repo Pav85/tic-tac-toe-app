94c3a4dc3fdde2735d9dd03a91462a18cac87f8f
Pawel <pawel.werbowy@gmail.com>
2024-07-17 15:34:26 +0100

    Update Board.js

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import Square from "../square/Square";
 
 const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (index) => {
+    const newSquares = squares.slice();
+    if (calculateWinner(squares) || squares[index]) {
+      return;
+    }
+    newSquares[index] = xIsNext ? "X" : "O";
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  };
+
   return <div>Board</div>;
 };
 
