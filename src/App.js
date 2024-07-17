40325c721d760882f74cb8da05dfd86f982856dc
Pawel <pawel.werbowy@gmail.com>
2024-07-17 16:09:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import Game from "./components/game/Game";
 import "./static/style/main.css";
 
 const App = () => {
-  return <Game />;
+  return (
+    <div>
+      <h1>Tic Tac Toe</h1>
+      <Game />;
+    </div>
+  );
 };
 
 export default App;
