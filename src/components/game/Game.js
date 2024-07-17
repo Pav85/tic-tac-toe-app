975f9e4fbb8d69e25ea4c5e593d00c2407abd842
Pawel <pawel.werbowy@gmail.com>
2024-07-17 16:04:13 +0100

    functionality achieved

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Board from "../board/Board";
 
 const Game = () => {
-  return <div>Game</div>;
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+    </div>
+  );
 };
 
 export default Game;
