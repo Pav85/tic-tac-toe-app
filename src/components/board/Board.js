d9fb201e539d634dc268f1508a27184230545752
Pawel <pawel.werbowy@gmail.com>
2024-07-17 15:56:50 +0100

    Update Board.js

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -15,7 +15,52 @@ const Board = () => {
     setXIsNext(!xIsNext);
   };
 
-  return <div>Board</div>;
+  const renderSquare = (index) => {
+    return (
+      <Square
+        key={index}
+        value={squares[index]}
+        onClick={() => handleClick(index)}
+      />
+    );
+  };
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${xIsNext ? "X" : "O"}`;
+  }
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board">
+        {squares.map((_, index) => renderSquare(index))}
+      </div>
+    </div>
+  );
+};
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let line of lines) {
+    const [a, b, c] = line;
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 };
 
 export default Board;
